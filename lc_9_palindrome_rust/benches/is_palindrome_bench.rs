7fbc61ebba3fac34b510430db26cc96faf54e14c
Misha Koshel <mouseratti@gmail.com>
2025-11-19 18:06:59 +0600

    lc_9_palindrome_rust: added benchmark comparison

--- lc_9_palindrome_rust/benches/is_palindrome_bench.rs
+++ lc_9_palindrome_rust/benches/is_palindrome_bench.rs
@@ -1,14 +1,25 @@
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
 use lc_9_palindrome_rust::is_palindrome;
 use lc_9_palindrome_rust::v2::is_palindrome_v2;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("is_palindrome_v2", |b| {
-        b.iter(|| is_palindrome(black_box(1234567890987654321)))
-    });
-}
+// pub fn criterion_benchmark(c: &mut Criterion) {
+//     c.bench_function("is_palindrome_v2", |b| {
+//         b.iter(|| is_palindrome(black_box(1234567890987654321)))
+//     });
+// }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_compare_functions(c: &mut Criterion) {
+    let mut group = c.benchmark_group("is_palindrome_benchgroup");
+    let inp = 123456789987654321;
+    group.bench_function(BenchmarkId::new("is_palindrome_v1", inp), |b| {
+        b.iter(|| is_palindrome(black_box(inp)))
+    });
+    group.bench_function(BenchmarkId::new("is_palindrome_v2", inp), |b| {
+        b.iter(|| is_palindrome_v2(black_box(inp)))
+    });
+    group.finish();
+}
+criterion_group!(benches, bench_compare_functions);
 criterion_main!(benches);
